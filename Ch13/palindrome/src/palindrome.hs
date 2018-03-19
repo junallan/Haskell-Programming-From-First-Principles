e95de6f2895f35d140a17af54fa9c5448211332f
Jun Allan Parreno <junallan@telus.net>
2018-03-18 21:08:21 -0600

    Palindrome modification to terminate on incorrect guess.

--- Ch13/palindrome/src/palindrome.hs
+++ Ch13/palindrome/src/palindrome.hs
@@ -1,10 +1,12 @@
 module Palindrome where
 
-import Control.Monad  
+import Control.Monad(forever)
+import System.Exit(exitSuccess)  
 
 palindrome :: IO ()
 palindrome = forever $ do
   line1 <- getLine
   case (line1 == reverse line1) of
     True -> putStrLn "It's a palindrome"
-    False -> putStrLn "Nope!"
+    False -> do putStrLn "Nope!"
+                exitSuccess
