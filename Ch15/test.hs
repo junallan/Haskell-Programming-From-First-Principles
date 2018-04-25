5961d3e166413c94b4e54fff16b7fd4a579b70f2
Jun Allan Parreno <junallan@telus.net>
2018-04-24 20:50:14 -0600

    Quick Check validation

--- Ch15/test.hs
+++ Ch15/test.hs
@@ -1,4 +1,11 @@
 import Data.Monoid
+import Test.QuickCheck
+
+monoidAssoc :: (Eq m, Monoid m)
+            => m -> m -> m -> Bool
+
+monoidAssoc a b c =
+  (a <> (b <> c)) == ((a <> b) <> c)
 
 data Optional a =
     Nada
@@ -9,6 +16,7 @@ instance Monoid a
       => Monoid (Optional a) where
   mempty = Nada
   mappend a b= a <> b
+
 
 
 --   instance Monoid b => Monoid (a -> b)
