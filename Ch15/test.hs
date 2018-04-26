b1d9f0653272d3c929b76b00147c77fa0378bb52
Jun Allan Parreno <junallan@telus.net>
2018-04-25 21:49:19 -0600

    Monoid quickcheck tests on left and right associativity

--- Ch15/test.hs
+++ Ch15/test.hs
@@ -1,5 +1,32 @@
+import Control.Monad
 import Data.Monoid
 import Test.QuickCheck
+
+data Bull = Fools | Twoo deriving (Eq, Show)
+
+instance Arbitrary Bull where
+  arbitrary = frequency [(1, return Fools), (1, return Twoo)]
+
+instance Monoid Bull where
+  mempty = Fools
+  mappend _ _ = Fools
+
+type BullMappend = Bull -> Bull -> Bull -> Bool
+
+main :: IO ()
+main = do
+  let ma = monoidAssoc
+      mli = monoidLeftIdentity
+      mlr = monoidRightIdentity
+  quickCheck (ma :: BullMappend)
+  quickCheck (mli :: Bull -> Bool)
+  quickCheck (mlr :: Bull -> Bool)
+
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
 
 monoidAssoc :: (Eq m, Monoid m)
             => m -> m -> m -> Bool
