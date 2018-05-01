b5904e7e83bdc66edebd687e6f6e726d6c36b8f9
Jun Allan Parreno <junallan@telus.net>
2018-04-30 20:55:53 -0600

    no message

--- Ch16/functors4.hs
+++ Ch16/functors4.hs
@@ -1,8 +1,26 @@
-data FixMePls a =
-    FixMe
-  | Pls a
+data WhoCares a =
+    ItDoesnt
+  | Matter a
+  | WhatThisIsCalled
   deriving (Eq, Show)
 
-instance Functor (FixMePls a) where
-  fmap _ FixMe = FixMe
-  fmap f (Pls a) = Pls (f a)
+instance Functor WhoCares where
+  fmap _ ItDoesnt = WhatThisIsCalled
+  fmap f WhatThisIsCalled = ItDoesnt
+  fmap f (Matter a) = Matter (f a)
+
+-- data CountingBad a =
+--   Heisenberg Int a
+--   deriving (Eq, Show)
+
+-- instance Functor CountingBad where
+--   fmap f (Heisenberg n a) =
+--     Heisenberg (n+1) (f a)
+
+data CountingGood a =
+  Heisenberg Int a
+  deriving (Eq, Show)
+
+instance Functor CountingGood where
+  fmap f (Heisenberg n a) =
+    Heisenberg (n) (f a)
